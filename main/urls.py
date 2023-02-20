5bdaff73c672c39f0d6975447518927f30388c18
akramakh <akram.icode@gmail.com>
2023-02-20 09:22:44 +0200

    fix typo in 'document_root' word

--- main/urls.py
+++ main/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('liprary.urls') )
 ] 
 
-urlpatterns += static(settings.STATIC_URL, documont_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, documont_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
